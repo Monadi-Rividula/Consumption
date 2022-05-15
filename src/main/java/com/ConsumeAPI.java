1011de44773f57c4ce3785152039c73a88015c3c
Monadi <IT20213244@my.sliit.lk>
2022-05-15 23:50:04 +0530

    Updated

---
+++ src/main/java/com/ConsumeAPI.java
@@ -0,0 +1,98 @@
+package com;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConsumeAPI
+ */
+@WebServlet("/ConsumeAPI")
+public class ConsumeAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Model itemObj = new Model(); 
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) 
+	{ 
+	 Map<String, String> map = new HashMap<String, String>(); 
+	try
+	 { 
+	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+	 String queryString = scanner.hasNext() ? 
+	 scanner.useDelimiter("\\A").next() : ""; 
+	 scanner.close(); 
+	 String[] params = queryString.split("&"); 
+	 for (String param : params) 
+	 {
+		 String[] p = param.split("=");
+		 map.put(p[0], p[1]); 
+		 } 
+		 } 
+		catch (Exception e) 
+		 { 
+		 } 
+		return map; 
+		}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	
+	
+    public ConsumeAPI() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String output = itemObj.insertItem(
+				  request.getParameter("month"), 
+				  request.getParameter("pastUnits"), 
+				  request.getParameter("currentUnits"), 
+				  request.getParameter("consumeUnits"),
+				   
+		
+		response.getWriter().write(output);
+	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request); 
+		 String output = itemObj.updateItem(paras.get("hidItemIDSave").toString(), 
+				 							paras.get("month").toString(), 
+				 							paras.get("pastUnits").toString(), 
+				 							paras.get("currentUnits").toString(), 
+				 							paras.get("consumeUnits").toString(), 
+				 							 
+		response.getWriter().write(output); 
+	}
+	
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		 String output = itemObj.deleteItem(paras.get("ServiceId").toString()); 
+		response.getWriter().write(output);
+	}
+
+}
